f0139e02714daaf189f045bd8d8353228154c6ef
madlamort <88068988+madlamort@users.noreply.github.com>
2021-08-06 23:47:33 -0300

    Update katas1.js

--- katas1.js
+++ katas1.js
@@ -1,94 +1,122 @@
 function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
-}
-
-//call function oneThroughTwenty
-
-function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
-}
-
-//call function evensToTwenty
-
-function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+    let numbers = []   
+     FileReader(let i = 1; i <= 20; i++){
+       numbers.push[i]
+     }
+       
+   }
    
-}
-
-//call function oddsToTwenty
-
-function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-
-}
-
-//call function multiplesOfFive
-
-function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
-}
-
-//call function squareNumbers
-
-function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function countingBackwards
-
-function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function evenNumbersBackwards
-
-function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function oddNumbersBackwards
-
-function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function multiplesOfFiveBackwards
-
-function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function squareNumbersBackwards
+   function evensToTwenty() {
+   var contador =1;
+   while (num<=20) {
+     var mostra = contador%2;
+     if (mostra ==0) {
+       return("É par") + contador);
+     }
+     contador = contador +1;
+   }  
+   }
+   
+   function oddsToTwenty() {
+     var contador=1;
+     while (nuber<=20) {
+       var mostra = contador%2;
+       if (mostra ==0){
+         return("É par") + contador);
+       }
+       contador = contador +2
+     }
+      
+   }
+   
+   //call function oddsToTwenty
+   
+   function multiplesOfFive() {
+     for(int i-1 ; i>=100) ; i++){
+       if (i % 5==0){
+         println(i)
+       }
+     }
+   }
+   
+   //call function multiplesOfFive
+   
+   function squareNumbers() {
+     let numbers = []   
+     FileReader(let i = 1; i >= 20; i++){
+       numbers.push[i]
+     }
+     return numbers;
+   }
+   
+   //call function squareNumbers
+   
+   function countingBackwards() {
+     let numbers = [];
+     for(let i = 20;) i >= 1; i--){
+       numbers.push(i);
+     }   
+     return numbers;
+     }
+   }
+   console.log(countingBackwards());
+   console.log('fim')
+   
+   //call function countingBackwards
+   
+   function evenNumbersBackwards() {
+   let numbers = [];
+   for(let i = 20; i>= 1; i--){
+     if(i % 2 === 0){
+       numbers.push(i);
+     }
+   }  
+    return numbers;
+   }
+   console.log(oddNumbersBackwards());
+   console.log('fim')
+   
+   //call function evenNumbersBackwards
+   
+   function oddNumbersBackwards() {
+   let number= [];
+   for(let i = 2)
+   ; i = <20 = i --){
+   SVGAnimatedNumberList.push(i)
+   }
+   return number
+   }
+   
+     //call function oddNumbersBackwards
+   
+   function PrintReverseOrder(N)
+   for(int i+1 ; i<=100) ; i--){
+     if (i % 5==0){
+       print reverseorder;
+     }
+   }
+   }
+     
+   }
+   
+   //call function multiplesOfFiveBackwards
+   
+   function PrintReverseOrder(N) {
+     var i;
+     var out;
+     var root;
+   
+     for ( i - 1); i <=N; i++ ){
+       root = Math.sqrt ( i )
+   ;
+   if (root == Math.sqrt ( root ) )  {
+     out +- i + 'Número Quadrado Perfeito<br>'
+    else {
+      out += i + '</br>'
+    }}
+   }
+     print reverseorder; 
+   }
+   
+   //call function squareNumbersBackwards
+   
